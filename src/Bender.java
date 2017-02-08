e29bd9d300f8d368323208e91e0855634ed5d0b3
Aleksey Gavrusev <lexa1907_98@mail.ru>
2017-02-08 19:14:26 +0400

    Initial commit

---
+++ src/Bender.java
@@ -0,0 +1,17 @@
+/**
+ * Created by NotePad.by on 07.02.2017.
+ */
+public class Bender {
+    public static void main(String[] args) {
+        // Display "Hello World!"
+          System.out.println("Hello World! Oleg");
+    System.out.println("OLeg is programming");
+    System.out.println(PI(3));
+    }
+    public static double PI(int a)
+    {
+
+        return a*3.14;
+
+    }
+    }
